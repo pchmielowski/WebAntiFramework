f5f3b25c1f58e9a4a7a0b9f3baa2393989fea9f9
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-01-22 11:32:24 +0100

    Indents

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -7,123 +7,123 @@ import java.net.ServerSocket
 import java.net.Socket
 
 fun main(args: Array<String>) {
-    val users = mutableListOf<String>()
-    Application(
-            "/hello" to TextPage(
-                    "Hello, %s %s!",
-                    Param("name"),
-                    Param("sur")
-            ),
-            "/goodbye" to TextPage(
-                    "Good bye! Be back on %s!",
-                    Param("back")
-            ),
-            "/html" to HtmlPage(
-                    HtmlTag("html",
-                            HtmlTag("body",
-                                    HtmlTag("center", "Hello")))
-            ),
-            "/add" to Action(
-                    Param("user"),
-                    { name -> users.add(name) },
-                    HtmlPage(
-                            HtmlTag("html",
-                                    HtmlTag("body",
-                                            listOf(
-                                                    HtmlTag("center", "OK"),
-                                                    HtmlTag("a", "click", params = "href=list")
-                                            ))))
-            ),
-            "/list" to HtmlPage(
-                    HtmlTag("html",
-                            HtmlTag("body",
-                                    HtmlTag("ol",
-                                            {
-                                                users.map { HtmlTag("li", it) }
-                                            })))
-            )).start(8080)
+  val users = mutableListOf<String>()
+  Application(
+      "/hello" to TextPage(
+          "Hello, %s %s!",
+          Param("name"),
+          Param("sur")
+      ),
+      "/goodbye" to TextPage(
+          "Good bye! Be back on %s!",
+          Param("back")
+      ),
+      "/html" to HtmlPage(
+          HtmlTag("html",
+              HtmlTag("body",
+                  HtmlTag("center", "Hello")))
+      ),
+      "/add" to Action(
+          Param("user"),
+          { name -> users.add(name) },
+          HtmlPage(
+              HtmlTag("html",
+                  HtmlTag("body",
+                      listOf(
+                          HtmlTag("center", "OK"),
+                          HtmlTag("a", "click", params = "href=list")
+                      ))))
+      ),
+      "/list" to HtmlPage(
+          HtmlTag("html",
+              HtmlTag("body",
+                  HtmlTag("ol",
+                      {
+                        users.map { HtmlTag("li", it) }
+                      })))
+      )).start(8080)
 }
 
 class Param(val name: String) {
-    fun value(request: IRequest): String {
-        return request.param(name)
-    }
+  fun value(request: IRequest): String {
+    return request.param(name)
+  }
 }
 
 class Application(vararg val endpoints: Pair<String, Response>) {
-    fun start(port: Int) {
-        val server = ServerSocket(port)
-        while (true) {
-            server.accept().use { socket ->
-                function(socket)
-            }
-        }
+  fun start(port: Int) {
+    val server = ServerSocket(port)
+    while (true) {
+      server.accept().use { socket ->
+        function(socket)
+      }
     }
+  }
 
-    private fun function(socket: Socket) {
-        val request = Request(socket.inputStream)
-        endpoints.toMap()[request.endpoint()]
-                ?.answer(request, socket)
-    }
+  private fun function(socket: Socket) {
+    val request = Request(socket.inputStream)
+    endpoints.toMap()[request.endpoint()]
+        ?.answer(request, socket)
+  }
 }
 
 fun param(url: String, name: String): String {
-    val value = (".*[\\?&]$name=(\\w+).*").toRegex()
-            .matchEntire(url)?.groups?.get(1)?.value ?: return ""
-    return value
+  val value = (".*[\\?&]$name=(\\w+).*").toRegex()
+      .matchEntire(url)?.groups?.get(1)?.value ?: return ""
+  return value
 }
 
 fun endpoint(url: String): String {
-    val value = ("GET\\s([^?^\\s]*)?.*").toRegex()
-            .matchEntire(url)?.groups?.get(1)?.value ?: return ""
-    return value
+  val value = ("GET\\s([^?^\\s]*)?.*").toRegex()
+      .matchEntire(url)?.groups?.get(1)?.value ?: return ""
+  return value
 }
 
 
 class Request(stream: InputStream) : IRequest {
-    val content = ctnt(stream)
+  val content = ctnt(stream)
 
-    private fun ctnt(stream: InputStream): String {
-        val reader = BufferedReader(
-                InputStreamReader(stream)
-        )
-        val lines = mutableListOf<String>()
-        do {
-            val line = reader.readLine()
-            lines.add(line)
-        } while (line != "")
-        return lines.joinToString()
-    }
+  private fun ctnt(stream: InputStream): String {
+    val reader = BufferedReader(
+        InputStreamReader(stream)
+    )
+    val lines = mutableListOf<String>()
+    do {
+      val line = reader.readLine()
+      lines.add(line)
+    } while (line != "")
+    return lines.joinToString()
+  }
 
-    override fun param(name: String): String = param(content, name)
-    fun endpoint(): String = endpoint(content)
+  override fun param(name: String): String = param(content, name)
+  fun endpoint(): String = endpoint(content)
 }
 
 interface IRequest {
-    fun param(name: String): String
+  fun param(name: String): String
 }
 
 class TextPage(val msg: String, vararg val param: Param) : Response {
-    override fun answer(request: IRequest, socket: Socket) {
-        socket.outputStream.write(
-                ("HTTP/1.1 200 OK\r\n\r\n" +
-                        msg.format(
-                                *param.map {
-                                    p ->
-                                    p.value(request)
-                                }.toTypedArray()
-                        )).toByteArray())
-    }
+  override fun answer(request: IRequest, socket: Socket) {
+    socket.outputStream.write(
+        ("HTTP/1.1 200 OK\r\n\r\n" +
+            msg.format(
+                *param.map {
+                  p ->
+                  p.value(request)
+                }.toTypedArray()
+            )).toByteArray())
+  }
 
 }
 
 
 class HtmlPage(val html: HtmlTag) : Response {
-    override fun answer(request: IRequest, socket: Socket) {
-        socket.outputStream.write(
-                ("HTTP/1.1 200 OK\r\n\r\n" +
-                        html.src()).toByteArray())
-    }
+  override fun answer(request: IRequest, socket: Socket) {
+    socket.outputStream.write(
+        ("HTTP/1.1 200 OK\r\n\r\n" +
+            html.src()).toByteArray())
+  }
 
 }
 
@@ -131,44 +131,44 @@ class HtmlPage(val html: HtmlTag) : Response {
 class Action(val param: Param,
              val function: (String) -> Unit,
              val response: Response) : Response {
-    override fun answer(request: IRequest, socket: Socket) {
-        val value = param.value(request)
-        function.invoke(value)
-        response.answer(request, socket)
-    }
+  override fun answer(request: IRequest, socket: Socket) {
+    val value = param.value(request)
+    function.invoke(value)
+    response.answer(request, socket)
+  }
 
 }
 
 
 class HtmlTag private constructor(
-        val tag: String,
-        val innerGenerator: () -> String,
-        val params: String = "",
-        i: Int = 0 // Dirty trick to distinct two function taking ctors
+    val tag: String,
+    val innerGenerator: () -> String,
+    val params: String = "",
+    i: Int = 0 // Dirty trick to distinct two function taking ctors
 ) {
-    fun src(): String {
-        return "<%1\$s%3\$s>%2\$s</%1\$s>".format(
-                tag,
-                innerGenerator(),
-                if (params == "") "" else " " + params)
-    }
+  fun src(): String {
+    return "<%1\$s%3\$s>%2\$s</%1\$s>".format(
+        tag,
+        innerGenerator(),
+        if (params == "") "" else " " + params)
+  }
 
-    constructor(tag: String) : this(tag, innerGenerator = { "" })
-    constructor(tag: String, inner: String, params: String = "") : this(
-            tag,
-            innerGenerator = { inner },
-            params = params)
+  constructor(tag: String) : this(tag, innerGenerator = { "" })
+  constructor(tag: String, inner: String, params: String = "") : this(
+      tag,
+      innerGenerator = { inner },
+      params = params)
 
-    constructor(tag: String, inner: HtmlTag) : this(tag, innerGenerator = { inner.src() })
-    constructor(tag: String, inners: List<HtmlTag>) : this(
-            tag,
-            innerGenerator = { inners.map(HtmlTag::src).joinToString(separator = "") }
-    )
+  constructor(tag: String, inner: HtmlTag) : this(tag, innerGenerator = { inner.src() })
+  constructor(tag: String, inners: List<HtmlTag>) : this(
+      tag,
+      innerGenerator = { inners.map(HtmlTag::src).joinToString(separator = "") }
+  )
 
-    constructor(tag: String, f: () -> List<HtmlTag>) : this(
-            tag,
-            innerGenerator = { f().map { it.src() }.joinToString("") }
-    )
+  constructor(tag: String, f: () -> List<HtmlTag>) : this(
+      tag,
+      innerGenerator = { f().map { it.src() }.joinToString("") }
+  )
 
 
 }
@@ -176,5 +176,5 @@ class HtmlTag private constructor(
 
 interface Response {
 
-    fun answer(request: IRequest, socket: Socket)
+  fun answer(request: IRequest, socket: Socket)
 }
--- src/main/Test.kt
+++ src/main/Test.kt
@@ -7,57 +7,57 @@ import org.junit.Test
 import java.net.Socket
 
 class Tests {
-    @Test fun endpoint_test() {
-        Assert.assertThat(endpoint("GET /?code=1234"), `is`("/"))
-    }
+  @Test fun endpoint_test() {
+    Assert.assertThat(endpoint("GET /?code=1234"), `is`("/"))
+  }
 
 
-    @Test fun param() {
-        Assert.assertThat(param("wp.pl?code=1234", "code"), `is`("1234"))
-        Assert.assertThat(param("wp.pl?code=abc", "code"), `is`("abc"))
-        Assert.assertThat(param("wp.pl?code=abc&another=xyz", "code"), `is`("abc"))
-        Assert.assertThat(param("127.0.0.1:8080?name=Piotrek", "name"), `is`("Piotrek"))
-    }
+  @Test fun param() {
+    Assert.assertThat(param("wp.pl?code=1234", "code"), `is`("1234"))
+    Assert.assertThat(param("wp.pl?code=abc", "code"), `is`("abc"))
+    Assert.assertThat(param("wp.pl?code=abc&another=xyz", "code"), `is`("abc"))
+    Assert.assertThat(param("127.0.0.1:8080?name=Piotrek", "name"), `is`("Piotrek"))
+  }
 
-    @Test fun html() {
-        Assert.assertThat(HtmlTag("html").src(), `is`("<html></html>"))
-        Assert.assertThat(HtmlTag("body").src(), `is`("<body></body>"))
-        Assert.assertThat(HtmlTag("html", HtmlTag("body")).src(), `is`("<html><body></body></html>"))
-        Assert.assertThat(
-                HtmlTag("html",
-                        HtmlTag("body",
-                                HtmlTag("center", "Hello"))
-                ).src(),
-                `is`("<html><body><center>Hello</center></body></html>"))
-        Assert.assertThat(
-                HtmlTag("body",
-                        listOf(
-                                HtmlTag("center", "Hello"),
-                                HtmlTag("left", "World"))
-                ).src(),
-                `is`("<body><center>Hello</center><left>World</left></body>"))
-        Assert.assertThat(HtmlTag("a", "link", params = "href='url'").src(), `is`("<a href='url'>link</a>"))
-    }
+  @Test fun html() {
+    Assert.assertThat(HtmlTag("html").src(), `is`("<html></html>"))
+    Assert.assertThat(HtmlTag("body").src(), `is`("<body></body>"))
+    Assert.assertThat(HtmlTag("html", HtmlTag("body")).src(), `is`("<html><body></body></html>"))
+    Assert.assertThat(
+        HtmlTag("html",
+            HtmlTag("body",
+                HtmlTag("center", "Hello"))
+        ).src(),
+        `is`("<html><body><center>Hello</center></body></html>"))
+    Assert.assertThat(
+        HtmlTag("body",
+            listOf(
+                HtmlTag("center", "Hello"),
+                HtmlTag("left", "World"))
+        ).src(),
+        `is`("<body><center>Hello</center><left>World</left></body>"))
+    Assert.assertThat(HtmlTag("a", "link", params = "href='url'").src(), `is`("<a href='url'>link</a>"))
+  }
 
-    @Ignore @Test fun action() {
-        Action(
-                Param("user"),
-                { p -> println(p) }, HtmlPage(
-                HtmlTag("html",
-                        HtmlTag("body",
-                                listOf(
-                                        HtmlTag("center", "OK"),
-                                        HtmlTag("a", "click")
-                                )
+  @Ignore @Test fun action() {
+    Action(
+        Param("user"),
+        { p -> println(p) }, HtmlPage(
+        HtmlTag("html",
+            HtmlTag("body",
+                listOf(
+                    HtmlTag("center", "OK"),
+                    HtmlTag("a", "click")
+                )
 
-                        ))
-        )
-        ).answer(MockedRequest(), Socket())
-    }
+            ))
+    )
+    ).answer(MockedRequest(), Socket())
+  }
 }
 
 class MockedRequest : IRequest {
-    override fun param(name: String): String = "Piotrek"
+  override fun param(name: String): String = "Piotrek"
 
 }
 

59fbff9ca426f7ef5251e9a87dd17bce0b7e2cbc
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-01-22 09:32:42 +0100

    Passing HtmlTag tests

--- src/main/Test.kt
+++ src/main/Test.kt
@@ -16,4 +16,25 @@ class Tests {
         Assert.assertThat(param("wp.pl?code=abc&another=xyz", "code"), `is`("abc"))
         Assert.assertThat(param("127.0.0.1:8080?name=Piotrek", "name"), `is`("Piotrek"))
     }
+
+    @Test fun html() {
+        Assert.assertThat(HtmlTag("html").src(), `is`("<html></html>"))
+        Assert.assertThat(HtmlTag("body").src(), `is`("<body></body>"))
+        Assert.assertThat(HtmlTag("html", HtmlTag("body")).src(), `is`("<html><body></body></html>"))
+        Assert.assertThat(
+                HtmlTag("html",
+                        HtmlTag("body",
+                                HtmlTag("center", "Hello"))
+                ).src(),
+                `is`("<html><body><center>Hello</center></body></html>"))
+    }
+}
+
+class HtmlTag(val tag: String, val inner: String) {
+    fun src(): String {
+        return "<%1\$s>%2\$s</%1\$s>".format(tag, inner)
+    }
+
+    constructor(tag: String) : this(tag, "")
+    constructor(tag: String, inner: HtmlTag) : this(tag, inner.src())
 }

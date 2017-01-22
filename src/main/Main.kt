1e68ad0315ea91aee01de3fb77d0ef7b957d8cc7
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-01-22 13:54:30 +0100

    Refactors in App

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -4,31 +4,34 @@ import framework.*
 
 fun main(args: Array<String>) {
   val users = mutableListOf<String>()
-  Application(
-      "/" to HtmlPage(
+
+  val mainPage = HtmlPage(
+      HtmlTag("body",
+          "bgcolor=gray",
+          HtmlTag("h1", "Users:"),
+          HtmlTag("ol",
+              {
+                users.map { user -> HtmlTag("li", user) }
+              }),
+          HtmlTag("form",
+              "action=add method=get",
+              HtmlTag("input", params = "name=user"),
+              HtmlTag("input", params = "type=submit value=Add")
+          )
+      ))
+
+  val add = Action(
+      Param("user") to { value -> users.add(value) },
+      response = HtmlPage(
           HtmlTag("body",
               "bgcolor=gray",
-              HtmlTag("h1", "Users:"),
-              HtmlTag("ol",
-                  {
-                    users.map { user -> HtmlTag("li", user) }
-                  }),
-              HtmlTag("form",
-                  "action=add method=get",
-                  HtmlTag("input", params = "name=user"),
-                  HtmlTag("input", params = "type=submit value=Add")
-              )
-          )),
-      "/add" to Action(
-          Param("user") to { value -> users.add(value) },
-          response = HtmlPage(
-              HtmlTag("body",
-                  "bgcolor=gray",
-                  HtmlTag("h1", "OK"),
-                  HtmlTag("a", params = "href=/", inner = "Back")
-              ))
-      )
+              HtmlTag("h1", "OK"),
+              HtmlTag("a", params = "href=/", inner = "Back")
+          ))
+  )
+  
+  Application(
+      "/" to mainPage,
+      "/add" to add
   ).start(8080)
 }
-
-

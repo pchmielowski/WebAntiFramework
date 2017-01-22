43b63736c5728af9481aa88d0b1f59bc0f32422e
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-01-22 12:46:55 +0100

    Test corrected

--- test.bash
+++ test.bash
@@ -4,8 +4,6 @@ function assert {
   curl "$1" 2>/dev/null | grep "$2"; echo $?
 }
 
-assert 'localhost:8080/hello?name=Piotrek&sur=chmielowski' 'Hello, Piotrek chmielowski!'
-
 assert 'localhost:8080/add?user=First' 'OK'
 assert 'localhost:8080/add?user=Second' 'OK'
-assert 'localhost:8080/list' '<inner><inner><ol><li>First</li><li>Second</li></ol></inner></inner>'
+assert 'localhost:8080' '<ol><li>First</li><li>Second</li></ol>'
